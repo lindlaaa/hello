6043e73108524f73d6682c3861f63d8ff42c5897
Avery Lindley <develop.lindley+git@gmail.com>
2019-04-04 20:54:24 -0500

    TravisCI

--- src/main/java/com/lindley/address/controller/hello.java
+++ src/main/java/com/lindley/address/controller/hello.java
@@ -3,12 +3,13 @@ package com.lindley.address.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class hello {
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> sayHello() {
         return new ResponseEntity<String>("Hello world!", HttpStatus.OK);
     }
